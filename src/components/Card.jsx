a7f9310c512ebcfb2987e95d38943b80398a453f
Bharat Rav <bharatrav8077@gmail.com>
2023-06-28 17:47:18 +0530

    whatsapp button added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,48 +1,15 @@
 import React from "react";
 import {
-  EmailShareButton,
   FacebookShareButton,
-  HatenaShareButton,
-  InstapaperShareButton,
-  LineShareButton,
   LinkedinShareButton,
-  LivejournalShareButton,
-  MailruShareButton,
-  OKShareButton,
-  PinterestShareButton,
-  PocketShareButton,
-  RedditShareButton,
-  TelegramShareButton,
-  TumblrShareButton,
   TwitterShareButton,
-  ViberShareButton,
-  VKShareButton,
-  WhatsappShareButton,
-  WorkplaceShareButton,
 } from "react-share";
 
 import {
-  EmailIcon,
   FacebookIcon,
-  FacebookMessengerIcon,
-  HatenaIcon,
-  InstapaperIcon,
-  LineIcon,
   LinkedinIcon,
-  LivejournalIcon,
-  MailruIcon,
-  OKIcon,
-  PinterestIcon,
-  PocketIcon,
-  RedditIcon,
-  TelegramIcon,
-  TumblrIcon,
   TwitterIcon,
-  ViberIcon,
-  VKIcon,
-  WeiboIcon,
   WhatsappIcon,
-  WorkplaceIcon,
 } from "react-share";
 const Card = ({ post }) => {
   const {
@@ -91,6 +58,15 @@ const Card = ({ post }) => {
           >
             <LinkedinIcon size={32} round={true} />
           </LinkedinShareButton>
+        </span>
+        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+          <WhatsappShareButton
+            url={images?.[0]}
+            title={title}
+            separator={description}
+          >
+            <WhatsappIcon size={32} round={true} />
+          </WhatsappShareButton>
         </span>
       </div>
     </div>
